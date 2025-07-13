bb40e9d115af4e8e77cdde5a6f0871ae770b28f9
Mounir LBATH <100532921+devmlGit@users.noreply.github.com>
2025-07-13 12:23:44 +0200

    add WebSocket functionality for client

---
+++ client.js
@@ -0,0 +1,22 @@
+
+const socket = new WebSocket('ws://localhost:8080');
+
+// when connection successful
+socket.addEventListener("open", (event) => {
+    console.log("WebSocket connection successfully established!")
+
+    socket.send("Hello serevr!")
+});
+
+
+socket.addEventListener("message", (event) => {
+    console.log("Message received from server:", event.data);
+});
+
+socket.addEventListener("close", (event) => {
+    console.log("Connection closed!")
+});
+
+socket.addEventListener("error", (error) => {
+    console.log("Error:", error);
+});
